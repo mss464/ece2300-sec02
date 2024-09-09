37c26582cd0c60afe9af0b11345ae6c60231c408
mss464 <mss464@cornell.edu>
2024-09-08 21:29:40 -0400

    my awesome pair triple detector

--- PairTripleDetector.v
+++ PairTripleDetector.v
@@ -9,7 +9,11 @@ module PairTripleDetector
   input  wire in2,
   output wire out
 );
-
+wire w,x,y;
+or(w,in0,in1);
+and(x,in0,in1);
+and(y,w,in2);
+or(out,y,x);
   // '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
   // Discussion Section Task
   // '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
